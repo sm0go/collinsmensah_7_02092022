00cdd6846c9c9920351a149121ae9f9e27d46282
Collins <collinsmensah@live.fr>
2022-09-05 15:13:11 +0200

    first commit

---
+++ backend/controller/post.js
@@ -0,0 +1,53 @@
+const Post = require('../models/Post')
+const Profile = require('../models/Profile')
+
+exports.getPosts = (req, res, next) => {
+  Post.find()
+    .then(posts => res.status(200).json(posts))
+    .catch(error => res.status(400).json({ error }))
+}
+
+exports.getOnePost = (req, res, next) => {
+  // console.log(req.params.id);
+  // res.send({message : 'ok'})
+  Post.findOne({ _id: req.params.id })
+  .then(posts => res.status(200).json(posts))
+  .catch(error => res.status(400).json({ error }))
+}
+
+exports.createPost = (req, res, next) => {
+  // const postParse = JSON.parse(req.body)
+  // delete postParse.userId 
+  // const post = new Post({
+  //   ...req.body,
+  //   userId: req.auth.userId,
+  //   pseudo: 'fsdfdsfs',
+  //   image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+  //   // likes: 0,
+  //   // dislikes: 0,
+  //   // usersLiked: [], 
+  //   // usersDisliked: []
+  // })
+  const newPost = new Post ({
+    ...req.body,
+    userId: req.auth.userId,
+    dateOfCreation: new Date(), 
+    image: req.body.file
+    // image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+  })
+  newPost.save()
+  .then(()=> {
+    res.status(201).json({ message: "Post saved !"})
+  })
+  .catch(error => res.status(400).json({ error }))
+}
+
+exports.modifyPost = (req, res, next) => {
+
+}
+
+exports.deletePost = (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id })
+    .then(posts => res.status(200).json(posts))
+    .catch(error => res.status(400).json({ error }))
+}
